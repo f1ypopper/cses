891beff4eba463b82c9396d1f399499ce7f8f8c5
Atharva Patil <patil8atharva@gmail.com>
2024-07-04 08:39:55 +0530

    Increasing Array

--- src/bin/increasing_array.rs
+++ src/bin/increasing_array.rs
@@ -7,10 +7,14 @@ fn main() {
         .unwrap()
         .unwrap()
         .split(' ')
-        .map(|x| x.parse::<u32>().unwrap())
+        .map(|x| x.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
-    let mut moves = 0;
+    let mut moves: usize = 0;
     for i in 0..array.len() - 1 {
+        if array[i] > array[i+1]{
+            moves+=array[i]-array[i+1];
+            array[i+1]+=array[i]-array[i+1];
+        }
     }
     println!("{moves}");
 }
